0c5a72a053890d98ff49392fd168c970a79e1f1a
Karthik Singh <karthiksingh@Karthiks-iMac.local>
2023-06-20 11:36:46 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import ForgotPassword from "./pages/ForgotPassword";
+import Signup from "./pages/Signup";
 
 
 function App() {
 
   return (
     <>
-     <h1 className='text-3xl underline'>Hello</h1>
+     <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="forgot-password" element={<ForgotPassword />} />
+        <Route path="offers" element={<Offers />} />
+      </Routes>
+     </Router>
     </>
   )
 }
